369156681db673a7e56abd3c18ef2ddda51c5d3a
Matthew Xuereb <matxuereb@hotmail.co.uk>
2016-03-28 19:20:40 +0200

    Added models Users and posts

---
+++ NetPress/Models/Posts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetPress.Models
+{
+    public class Posts
+    {
+        public int postID { get; set; }
+        public string title { get; set; }
+
+        public string content { get; set; }
+
+        public string category { get; set; }
+
+        public int authorID { get; set; }
+
+        public string status{ get; set; }
+
+        public string dateCreated { get; set; }
+
+        public string lastModified{ get; set; }
+
+ 
+
+
+    }
+}
