a470d700053ddfd535f8e8df58c9c652e5a7795b
Samuel Borg <yellowcrown@gmail.com>
2016-03-29 11:49:07 +0200

    Fixed DAL path

---
+++ NetPress/Models/NetPressContext.cs
@@ -0,0 +1,23 @@
+﻿using NetPress.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace NetPress.DAL
+{
+    public class NetPressContext : DbContext
+    {
+
+        public NetPressContext() : base("NetPressContext")
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
