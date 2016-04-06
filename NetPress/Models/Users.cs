92bb28781b7ffbde348f879cd66086fea71d13b4
Samuel Borg <yellowcrown@gmail.com>
2016-04-06 18:30:29 +0200

    Revert "Revert "Removed Posts and created Post with model changes. Added created and modified dates, HTML editor and validation.""
    This reverts commit 13d5d28aca755c186c329b2ed1962924764b3b82.

--- NetPress/Models/Users.cs
+++ NetPress/Models/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace NetPress.Models
 {
     public class Users
     {
+        [Key]
         public int userID { get; set; }
 
         public string username { get; set; }
